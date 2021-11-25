547fbc16f9a71004011fadf5f99c722b61f8d1b8
Zack McKevitt <zack.mckevitt@gmail.com>
2021-11-25 02:04:11 -0700

    First commit

---
+++ tennis.c
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+//////////////////////////////////////////////////////////////
+//
+// MATH
+//
+//////////////////////////////////////////////////////////////
+
+struct vec {
+    float x;
+    float y;
+};
+
+// Do we need a constructor?
+struct vec init_vec(float x, float y) {
+    struct vec v;
+    v.x = x;
+    v.y = y;
+
+    return v;
+}
+
+struct vec vec_sum(struct vec v1, struct vec v2) {
+    struct vec v;
+
+    v.x = v1.x + v2.x;
+    v.y = v1.y + v2.y;
+
+    return v;
+}
+
+float vec_length(struct vec v) {
+    return sqrt(v.x*v.x + v.y*v.y);
+}
+
+struct vec flip_x(struct vec v) {
+    struct vec tmp;
+
+    tmp.x = -1 * v.x;
+    tmp.y = v.y;
+
+    return tmp;
+}
+
+struct vec flip_y(struct vec v) {
+    struct vec tmp;
+
+    tmp.x = v.x;
+    tmp.y = -1 * v.y;
+
+    return tmp;
+}
+
+struct vec flip(struct vec v) {
+    struct vec tmp;
+
+    tmp.x = -1 * v.x;
+    tmp.y = -1 * v.y;
+
+    return tmp;
+}
+
+struct vec diff(struct vec v1, struct vec v2) {
+    struct vec tmp;
+
+    tmp.x = v2.x - v1.x;
+    tmp.y = v2.y - v1.y;
+
+    return tmp;
+}
+
+struct vec scale(struct vec v, float sf) {
+    struct vec tmp;
+
+    tmp.x = v.x * sf;
+    tmp.y = v.y * sf;
+
+    return tmp;
+}
+
+// Transform function?
+
+//////////////////////////////////////////////////////////////
+//
+// Graphics
+//
+//////////////////////////////////////////////////////////////
+
+#define HEIGHT 40
+#define WIDTH 64
+#define FPS 20
+
+
+// returns cursor to starting position
+static void back(void)
+{
+    printf("\x1b[%dD", WIDTH);
+    printf("\x1b[%dA", HEIGHT);
+}
+
+// draws picture with character at position
+int draw(struct vec pos, int last1, int last2) {
+
+    // make these dependent on height/width or something
+    struct vec guy1 = init_vec(16,4);
+    struct vec guy2 = init_vec(WIDTH - 16 - 1, HEIGHT - 4 -1);
+
+    char row[WIDTH];
+
+    int scalex = WIDTH/8;
+    int quit = 0;
+    for(int y=0; y<HEIGHT; ++y) {
+        for(int x=0; x<WIDTH; ++x) {
+            if(x == 0 || y == 0 || x == WIDTH - 1 || y == HEIGHT -1) {
+                row[x] = '#';
+            }
+            else if(y==HEIGHT/2) {
+                row[x] = '=';
+            }
+            else if ((x == WIDTH/2 - 1 || x == WIDTH/2) && (y > HEIGHT/2 - HEIGHT/4 && y < HEIGHT/2 + HEIGHT/4)) {
+                row[x] = '|';
+            }
+            else if (x == scalex || x == WIDTH - scalex -1) {
+                row[x] = '|';
+            }
+            else if ((y == HEIGHT/2 - HEIGHT/4 || y == HEIGHT/2 + HEIGHT/4) && (x < WIDTH - scalex -1 && x > scalex)) {
+                row[x] = '-';
+            }
+            else if(pos.x == x && pos.y == y) {
+                if(pos.x < scalex || pos.x > WIDTH-scalex-1) {
+                    row[x] = 'X';
+                    quit = 1;
+                }
+                else {
+                    row[x] = '@';
+                }
+            }
+            else if(guy1.x == x && guy1.y == y) {
+                if(last1){
+                    row[x] = '\\';
+                }
+                else {
+                    row[x] = '/';
+                }
+            }
+            else if(guy2.x == x && guy2.y == y) {
+                if(last2) {
+                    row[x] = '/';
+                }
+                else {
+                    row[x] = '\\';
+                }
+            }
+            else {
+                row[x] = ' ';
+            }
+        }
+        fwrite(row, WIDTH, 1, stdout);
+        fputc('\n', stdout);
+        
+    }
+    // if(quit)
+    //     return 1;
+    return 0;
+}
+
+// Ideas:
+//  Add DEPTH to ball. Increase in ASCII from low light to high light . -> @
+//  Movable characters
+int main(int argc, char** argv) {
+
+    struct vec guy1 = init_vec(15,4);
+    struct vec guy2 = init_vec(WIDTH - 15 - 1, HEIGHT - 4 -1);
+
+    // give guy1 the ball first
+    struct vec pos = guy1;
+
+    // choose random initial velocity
+    struct vec vel = init_vec(1,1);
+
+    int last1 = 1;
+    int last2 = 1;
+
+    while(1) {
+        pos = vec_sum(pos, vel);
+        if(((pos.x >= guy1.x - 2 && pos.x <= guy1.x + 2) && pos.y == guy1.y)
+            || ((pos.x <= guy2.x + 2 && pos.x >= guy2.x - 2) && pos.y == guy2.y)) {
+            vel = flip(vel);
+            last1 = !last1;
+            last2 = !last2;
+        }
+        if(pos.x >= WIDTH-1 || pos.x <= 1) {
+            vel = flip_x(vel);
+        }
+        if(pos.y >= HEIGHT-1 || pos.y <= 1) {
+            vel = flip_y(vel);
+        }
+        int stat = draw(pos, last1, last2);
+        if(stat) {
+            printf("OUT!\n");
+            return 0;
+        }
+        back();
+        usleep(1000 * 1000 / FPS);
+    }
+    return 0;
+}
